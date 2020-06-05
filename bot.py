ce298ef5bb9dbde7ca9990c05b469dc4e58be57b
Arhkei <54872004+Arhkei@users.noreply.github.com>
2020-06-05 13:41:22 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,47 @@
+import discord
+from googletrans import Translator
+# create discord client
+client = discord.Client()
+translator = Translator()
+
+# token from https://discordapp.com/developers
+token = 'NzE4NTQzOTM5MTE1ODEwOTI2.XtqbOw.WJVvaia-jsyUPWM2tnbWBnaK1BA'
+translation_channel = 718543716482023505
+general_channel = 718347361151090710
+# bot is ready
+@client.event
+async def on_ready():
+	try:
+		print(client.user.name)
+		print(client.user.id)
+		print('Discord.py Version: {}'.format(discord.__version__))
+
+	except Exception as e:
+
+		print(e)
+
+@client.event
+
+async def on_message(message):
+	# if the message is from the bot itself ignore it
+	if message.author == client.user:
+		pass
+	else:
+		detection = translator.detect(message.content).lang
+		if detection == 'en':
+			if str(message.channel) != "translation":
+				translation = translator.translate(message.content, dest='zh-CN').text
+				channel = client.get_channel(translation_channel)
+			else:
+				translation = message.content
+				channel = client.get_channel(general_channel)
+		elif detection == 'zh-CN':
+			if str(message.channel) != "general":
+				translation = translator.translate(message.content, dest='en').text
+				channel = client.get_channel(general_channel)
+			else:
+				translation = message.content
+				channel = client.get_channel(translation_channel)
+		name = str(message.author)[:-5]
+		await channel.send(f"{name}: {translation}")
+client.run(token)
